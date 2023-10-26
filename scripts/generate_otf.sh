c15ed3862c17f9c0cfbe544314179cff5236498d
Sim Domingo <sim@hersho.com>
2023-10-27 03:49:35 +0800

    install shfmt and use it to reformat build scripts

--- scripts/generate_otf.sh
+++ scripts/generate_otf.sh
@@ -7,10 +7,10 @@ set -euo pipefail
 # If not, print usage and exit
 
 if [ "$#" -ne 2 ]; then
-    echo "Generate OTF font from SFD source font"
-    echo "Usage: $0 <input.sfd> <output.otf>"
-    exit 1
+	echo "Generate OTF font from SFD source font"
+	echo "Usage: $0 <input.sfd> <output.otf>"
+	exit 1
 fi
 
 # Generate OTF font from SFD font
-fontforge -c 'font = open(argv[1]); font.generate(argv[2])' "$1" "$2" 
+fontforge -c 'font = open(argv[1]); font.generate(argv[2])' "$1" "$2"
